a23893db2221f25f7f1426c9913130e48ef81cd7
shivam <shivam88629@gmail.com>
2024-10-03 20:39:24 +0530

    firstCommit

---
+++ config/db.config.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+module.exports = {
+    HOST:'127.0.0.1' ,
+    USER: "root",
+    PASSWORD: 1234,
+    DB: "TestDB",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
